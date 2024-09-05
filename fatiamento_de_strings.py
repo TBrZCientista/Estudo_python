631f4e057a194a9f42a210d16986564625f99bdc
Felipe Freitas <felipefreitasdaschagas@hotmail.com>
2024-09-05 16:39:07 -0300

    aula sobre fatiamento de string

---
+++ fatiamento_de_strings.py
@@ -0,0 +1,15 @@
+nome = "Felipe Freitas das Chagas"
+
+print (nome[0]) #pega a primeira letra.
+
+print (nome[:9]) #pega todos os espaços até o "stop" que é o segundo parâmetro
+
+print (nome[10:]) #delimita de onde começa a captação de letras
+                  #como o parâmetro "stop" não foi indicado, pega até o final da string  
+print (nome[10:16]) #aqui temos o "start" e "stop" indicados
+
+print (nome[10:16:2]) #pega letra com a quantidade de "passos" indicados
+
+print (nome[:]) #pega a string inteira
+
+print (nome[::-1]) #escreve o nome ao contrário
