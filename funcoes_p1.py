906a681e066aa6e55af283ad60e1650fa26fd7f9
Felipe Freitas <felipefreitasdaschagas@hotmail.com>
2024-09-09 09:29:27 -0300

    segunda aula sobre funções

--- funcoes_p1.py
+++ funcoes_p1.py
@@ -37,5 +37,4 @@ def salvar_carro(marca, modelo, ano, placa):
 salvar_carro("Fiat","Palio",1999, "ABC-1234")
 salvar_carro(marca="Fiat", modelo="Palio", ano=1999, placa="ABC-1234")
 salvar_carro(**{"marca": "Fiat","modelo": "Palio", "ano": 1999, "placa": "ABC-1234"})
-#O ** é um dicionário usado como argumento.
-
+#O ** é um dicionário usado como argumento
